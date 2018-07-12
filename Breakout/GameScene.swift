09fb4c0a80e5a4d976ee442cb60e21016c8c6c8b
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-12 10:09:21 -0500

    Paddle moves on touch + drag

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -96,5 +96,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(loseZone)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
     
 }
